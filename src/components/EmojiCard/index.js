a27b42b01ebeb7eab1d723a33694cb11e902985d
tharun545 <tharun2k12@gmail.com>
2023-03-02 15:38:45 +0000

    Emoji Game Solution

---
+++ src/components/EmojiCard/index.js
@@ -0,0 +1,29 @@
+// Write your code here.
+
+import './index.css'
+import {Component} from 'react'
+
+class EmojiCard extends Component {
+  render() {
+    const {emoji, onClickEmoji} = this.props
+    const {emojiName, emojiUrl, id} = emoji
+    /* onClickEmojiItem sends clicked emoji id to EmojiGame main page  */
+    const onclickEmojiItem = () => {
+      onClickEmoji(id)
+    }
+    return (
+      <li className="emoji-item">
+        <button onClick={onclickEmojiItem} className="button">
+          <img
+            key={id}
+            src={emojiUrl}
+            alt={emojiName}
+            className="emoji-image"
+          />
+        </button>
+      </li>
+    )
+  }
+}
+
+export default EmojiCard
